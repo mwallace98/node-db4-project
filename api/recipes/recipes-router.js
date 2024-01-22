c6af637598a0970270e0e629f8fcbb8b115f1d2c
mwallace98 <mattwallace140@gmail.com>
2024-01-21 19:28:42 -0500

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -5,7 +5,6 @@ const Recipe = require('./recipes-model')
 router.get('/:recipe_id', (req,res,next) => {
     Recipe.getRecipeById(req.params.recipe_id)
     .then(resource => {
-        throw new Error('error')
         res.status(200).json(resource)
     })
     .catch(next)
